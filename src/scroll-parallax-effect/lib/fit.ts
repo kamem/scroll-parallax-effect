83bf65a00bec61f750b001df6ebeac3659f7677b
kamem <develo0@gmail.com>
2023-12-24 13:08:37 +0900

    @ts-ignoreがあるため/ prettier-ignoreを追加してpretterを実行

--- src/scroll-parallax-effect/lib/fit.ts
+++ src/scroll-parallax-effect/lib/fit.ts
@@ -2,182 +2,201 @@ import {
   getStyleValues,
   generateStyleValue,
   generateStyleValueString,
-  scrollPositionStringToNumber
-} from '../utils/util'
-import { easing } from '../utils/easing'
-import type {
-  TriggerPosiiton,
-  CSSStyleDeclarationName
-} from '../utils/util'
-import type {  Easing, EasingFunction } from '../utils/easing'
-import type ScrollStatus from './scrollStatus'
-type Ele = Element | HTMLElement | null
+  scrollPositionStringToNumber,
+} from '../utils/util';
+import { easing } from '../utils/easing';
+import type { TriggerPosiiton, CSSStyleDeclarationName } from '../utils/util';
+import type { Easing, EasingFunction } from '../utils/easing';
+import type ScrollStatus from './scrollStatus';
+type Ele = Element | HTMLElement | null;
 
-type MotionStyles =  {
-  [key in CSSStyleDeclarationName]?: string | number
-}
-type MotionValues =  {
-  [key in CSSStyleDeclarationName]?: number[]
-}
+type MotionStyles = {
+  [key in CSSStyleDeclarationName]?: string | number;
+};
+type MotionValues = {
+  [key in CSSStyleDeclarationName]?: number[];
+};
 export interface Motion {
-  start?: TriggerPosiiton
-  end: TriggerPosiiton
-  fromStyle?: MotionStyles
-  toStyle?: MotionStyles
-  easing?: Easing | EasingFunction
-  fromStyleValues?: MotionValues
-  toStyleValues?: MotionValues
+  start?: TriggerPosiiton;
+  end: TriggerPosiiton;
+  fromStyle?: MotionStyles;
+  toStyle?: MotionStyles;
+  easing?: Easing | EasingFunction;
+  fromStyleValues?: MotionValues;
+  toStyleValues?: MotionValues;
 }
 
 export default class Fit {
-  el?: Ele
-  motions: Motion[]
-  rangeMotions: Motion[]
-  styleValues: MotionStyles
+  el?: Ele;
+  motions: Motion[];
+  rangeMotions: Motion[];
+  styleValues: MotionStyles;
 
   constructor(el?: Ele) {
-    this.el = typeof el === 'string' ? document.querySelector(el) : el
-    this.styleValues = {}
-    this.motions = []
-    this.rangeMotions = []
+    this.el = typeof el === 'string' ? document.querySelector(el) : el;
+    this.styleValues = {};
+    this.motions = [];
+    this.rangeMotions = [];
   }
   setMotion(motion: Motion | Motion[]) {
-    const m = Array.isArray(motion) ? motion : [motion]
-    this.motions = m.map(motion => {
-      const fromStyle = this.setStyleValue(motion.fromStyle)
-      const toStyle = this.setStyleValue(motion.toStyle)
+    const m = Array.isArray(motion) ? motion : [motion];
+    this.motions = m.map((motion) => {
+      const fromStyle = this.setStyleValue(motion.fromStyle);
+      const toStyle = this.setStyleValue(motion.toStyle);
       return Object.assign({}, motion, {
         easing: motion.easing || 'linear',
         fromStyle,
         toStyle,
-      })
-    })
+      });
+    });
   }
 
   setStyleValues() {
-    this.motions = this.motions.map((motion) => Object.assign({}, motion, ({
-      fromStyleValues: this.generateStyleValues(motion.fromStyle),
-      toStyleValues: this.generateStyleValues(motion.toStyle)
-    })))
+    this.motions = this.motions.map((motion) =>
+      Object.assign({}, motion, {
+        fromStyleValues: this.generateStyleValues(motion.fromStyle),
+        toStyleValues: this.generateStyleValues(motion.toStyle),
+      })
+    );
   }
 
   generateStyleValues(motionStyles?: MotionStyles) {
-    let styles: MotionValues = {}
+    let styles: MotionValues = {};
     for (let style in motionStyles) {
-      const styleName = style as CSSStyleDeclarationName
-      styles[styleName] = getStyleValues(motionStyles[styleName]!.toString())
+      const styleName = style as CSSStyleDeclarationName;
+      styles[styleName] = getStyleValues(motionStyles[styleName]!.toString());
     }
-    return styles
+    return styles;
   }
 
   setStyleValue(motionStyles?: MotionStyles) {
-    let styles: MotionStyles = {}
+    let styles: MotionStyles = {};
     for (let style in motionStyles) {
-      const styleName = style as CSSStyleDeclarationName
-      styles[styleName] = generateStyleValue(motionStyles[styleName])
+      const styleName = style as CSSStyleDeclarationName;
+      styles[styleName] = generateStyleValue(motionStyles[styleName]);
     }
-    return styles
+    return styles;
   }
 
   setRangeMotions(status: ScrollStatus) {
-    const range: Motion[] = []
+    const range: Motion[] = [];
     this.motions.forEach((motion) => {
-      const start = scrollPositionStringToNumber(motion.start, status)
-      if (start <= status.scrollPosition) range.push(motion)
-    })
+      const start = scrollPositionStringToNumber(motion.start, status);
+      if (start <= status.scrollPosition) range.push(motion);
+    });
 
-    this.rangeMotions = range
+    this.rangeMotions = range;
   }
 
   setDefaultStyles() {
-    let defaultStyles: MotionStyles = {}
+    let defaultStyles: MotionStyles = {};
     this.motions.forEach(({ fromStyle }) => {
       for (let style in fromStyle) {
-        const styleName = style as CSSStyleDeclarationName
-        if (defaultStyles[styleName] === undefined) defaultStyles[styleName] = fromStyle[styleName]
+        const styleName = style as CSSStyleDeclarationName;
+        if (defaultStyles[styleName] === undefined)
+          defaultStyles[styleName] = fromStyle[styleName];
       }
-    })
-    this.styleValues = defaultStyles
+    });
+    this.styleValues = defaultStyles;
   }
 
   setFromStyle() {
     this.motions.forEach(({ fromStyle, toStyle }, i) => {
       for (let style in toStyle) {
-        const styleName = style as CSSStyleDeclarationName
-        if (fromStyle === undefined) fromStyle = {}
+        const styleName = style as CSSStyleDeclarationName;
+        if (fromStyle === undefined) fromStyle = {};
         if (fromStyle[styleName] === undefined) {
-          fromStyle[styleName] = this.getLastToStyle(styleName, i)
+          fromStyle[styleName] = this.getLastToStyle(styleName, i);
         }
       }
-    })
+    });
   }
   getLastToStyle(style: CSSStyleDeclarationName, i: number) {
-    let fromStyle = ''
-    const k = Math.max(i - 1, 0)
+    let fromStyle = '';
+    const k = Math.max(i - 1, 0);
     for (let j = k; j >= 0; j--) {
-      const motion = this.motions[j]
+      const motion = this.motions[j];
       if (motion.fromStyle![style] !== undefined) {
-        fromStyle = motion.toStyle![style]!.toString()
-        break
+        fromStyle = motion.toStyle![style]!.toString();
+        break;
       }
     }
     // @ts-ignore
+    // prettier-ignore
     if (fromStyle === '') fromStyle = document.defaultView.getComputedStyle(typeof this.el === 'string' ? document.querySelector(this.el) : this.el, null)[style]
 
-    return fromStyle
+    return fromStyle;
   }
 
   setStart() {
     this.motions.forEach((motion, i) => {
       if (motion.start === undefined) {
-        motion.start = this.getLastStart(i) || 0
+        motion.start = this.getLastStart(i) || 0;
       }
-    })
+    });
   }
   getLastStart(i: number) {
-    let start: TriggerPosiiton = ''
-    const k = Math.max(i - 1, 0)
+    let start: TriggerPosiiton = '';
+    const k = Math.max(i - 1, 0);
     for (let j = k; j >= 0; j--) {
-      const motion = this.motions[j]
+      const motion = this.motions[j];
       if (motion.start !== undefined) {
-        start = motion.end
-        break
+        start = motion.end;
+        break;
       }
     }
 
-    return start
+    return start;
   }
 
-  generateScrollStyleValues(style: string, fromtStyle: number, toStyle: number, easingName: string | Easing = 'linear', scrollPercent: number) {
-    const abs = Math.abs(fromtStyle - toStyle)
-    const fixAbs = fromtStyle < toStyle ? abs : -abs
-    const e = typeof easingName === 'string' ? easing[easingName as (keyof typeof easing)] as EasingFunction : easingName
-    let styleValue: number = e(scrollPercent, fromtStyle, fixAbs, 1)
+  generateScrollStyleValues(
+    style: string,
+    fromtStyle: number,
+    toStyle: number,
+    easingName: string | Easing = 'linear',
+    scrollPercent: number
+  ) {
+    const abs = Math.abs(fromtStyle - toStyle);
+    const fixAbs = fromtStyle < toStyle ? abs : -abs;
+    const e =
+      typeof easingName === 'string'
+        ? (easing[easingName as keyof typeof easing] as EasingFunction)
+        : easingName;
+    let styleValue: number = e(scrollPercent, fromtStyle, fixAbs, 1);
     if (style.indexOf('rgb') >= 0) {
-      styleValue = styleValue >= 1 ? Math.floor(styleValue) : styleValue < 0 ? 0 : styleValue;
+      styleValue =
+        styleValue >= 1
+          ? Math.floor(styleValue)
+          : styleValue < 0
+          ? 0
+          : styleValue;
     }
-    return styleValue
+    return styleValue;
   }
 
   getStyleValues(status: ScrollStatus) {
-    const { scrollPosition } = status
+    const { scrollPosition } = status;
     this.rangeMotions.forEach((motion, j) => {
-      const start = scrollPositionStringToNumber(motion.start, status)
-      const end = scrollPositionStringToNumber(motion.end, status)
-      const isInRange = start < scrollPosition && scrollPosition < end
-      const range = end - start
+      const start = scrollPositionStringToNumber(motion.start, status);
+      const end = scrollPositionStringToNumber(motion.end, status);
+      const isInRange = start < scrollPosition && scrollPosition < end;
+      const range = end - start;
 
-      const scrollPercent = isInRange ? (scrollPosition - start) / range :
-        (scrollPosition > start) ? 1 :
-          (scrollPosition < end) ? 0 : 0
+      const scrollPercent = isInRange
+        ? (scrollPosition - start) / range
+        : scrollPosition > start
+        ? 1
+        : scrollPosition < end
+        ? 0
+        : 0;
 
       for (let style in motion.fromStyle) {
-        const styleName = style as CSSStyleDeclarationName
-        const fromStyleValue = motion.fromStyle[styleName]!.toString()
-        const fromStyleValues = motion.fromStyleValues![styleName]
-        const toStyleValues = motion.toStyleValues![styleName]
+        const styleName = style as CSSStyleDeclarationName;
+        const fromStyleValue = motion.fromStyle[styleName]!.toString();
+        const fromStyleValues = motion.fromStyleValues![styleName];
+        const toStyleValues = motion.toStyleValues![styleName];
 
-        const values: number[] = []
+        const values: number[] = [];
         for (let i = 0; i < fromStyleValues!.length; i++) {
           values[i] = this.generateScrollStyleValues(
             fromStyleValue,
@@ -185,13 +204,16 @@ export default class Fit {
             toStyleValues![i],
             motion.easing,
             scrollPercent
-          )
+          );
         }
 
-        this.styleValues[styleName] = generateStyleValueString(fromStyleValue, values)
+        this.styleValues[styleName] = generateStyleValueString(
+          fromStyleValue,
+          values
+        );
       }
-    })
+    });
 
-    return this.styleValues
+    return this.styleValues;
   }
 }
--- src/scroll-parallax-effect/lib/scrollStatus.ts
+++ src/scroll-parallax-effect/lib/scrollStatus.ts
@@ -1,132 +1,140 @@
-const requestAnimationFrame = window.requestAnimationFrame
+const requestAnimationFrame = window.requestAnimationFrame;
 
-type Ele = Element | HTMLElement
-type Direction = 'y' | 'x'
-type Stage = typeof globalThis | Window | Ele
-export type DirectionPositionName = 'Top' | 'Left'
-export type StageSizeName = 'Height' | 'Width'
-type ScrollName = 'pageYOffset' | 'pageXOffset' |'scrollTop' | 'scrollLeft'
+type Ele = Element | HTMLElement;
+type Direction = 'y' | 'x';
+type Stage = typeof globalThis | Window | Ele;
+export type DirectionPositionName = 'Top' | 'Left';
+export type StageSizeName = 'Height' | 'Width';
+type ScrollName = 'pageYOffset' | 'pageXOffset' | 'scrollTop' | 'scrollLeft';
 
 export interface StatusParams {
-  stage?: Stage
-  direction?: Direction
-  functions?: ([(status: ScrollStatus) => void, ScrollPosition?])[]
-  targetPercentage?: number
-  threshold?: number
-  updateFunction?: (status: ScrollStatus) => void
+  stage?: Stage;
+  direction?: Direction;
+  functions?: [(status: ScrollStatus) => void, ScrollPosition?][];
+  targetPercentage?: number;
+  threshold?: number;
+  updateFunction?: (status: ScrollStatus) => void;
 }
 
 export default class ScrollStatus {
-  stage: Stage
-  direction: Direction
-  functions: ([(status: ScrollStatus) => void, ScrollPosition?])[]
-  targetPercentage: number
-  scrollPosition: number
-  endScrollPosition: number
-  threshold?: number
-  ScrollPosition: ScrollPosition
-  updateFunction?: (status: ScrollStatus) => void
-  stageSize: number
-  contentSize: number
-  stageSizeName?: StageSizeName
-  directionPositionName: DirectionPositionName
-  
+  stage: Stage;
+  direction: Direction;
+  functions: [(status: ScrollStatus) => void, ScrollPosition?][];
+  targetPercentage: number;
+  scrollPosition: number;
+  endScrollPosition: number;
+  threshold?: number;
+  ScrollPosition: ScrollPosition;
+  updateFunction?: (status: ScrollStatus) => void;
+  stageSize: number;
+  contentSize: number;
+  stageSizeName?: StageSizeName;
+  directionPositionName: DirectionPositionName;
+
   constructor() {
-    this.stage = globalThis || window
-    this.stageSize = 0
-    this.contentSize = 0
-    this.direction = 'y'
-    this.directionPositionName = 'Top'
-    this.functions = []
-    this.targetPercentage = 0.2
-    this.setDirectionInfo()
-    this.ScrollPosition = new ScrollPosition(this)
-    this.scrollPosition = this.ScrollPosition.generateScrollPosition()
-    this.endScrollPosition = this.ScrollPosition.endScrollPosition
-    this.scrollEventUpdate()
+    this.stage = globalThis || window;
+    this.stageSize = 0;
+    this.contentSize = 0;
+    this.direction = 'y';
+    this.directionPositionName = 'Top';
+    this.functions = [];
+    this.targetPercentage = 0.2;
+    this.setDirectionInfo();
+    this.ScrollPosition = new ScrollPosition(this);
+    this.scrollPosition = this.ScrollPosition.generateScrollPosition();
+    this.endScrollPosition = this.ScrollPosition.endScrollPosition;
+    this.scrollEventUpdate();
   }
   setVal(opt: StatusParams) {
-    this.stage = opt.stage ? opt.stage : globalThis || window
-    this.direction = opt.direction || this.direction
-    this.targetPercentage = opt.targetPercentage || 0.2
-    this.updateFunction = opt.updateFunction
-    this.threshold = opt.threshold || 0
-    this.ScrollPosition = new ScrollPosition(this)
-    this.scrollPosition = this.ScrollPosition.generateScrollPosition()
-    this.endScrollPosition = this.ScrollPosition.endScrollPosition
-    this.setDirectionInfo()
-    return this
+    this.stage = opt.stage ? opt.stage : globalThis || window;
+    this.direction = opt.direction || this.direction;
+    this.targetPercentage = opt.targetPercentage || 0.2;
+    this.updateFunction = opt.updateFunction;
+    this.threshold = opt.threshold || 0;
+    this.ScrollPosition = new ScrollPosition(this);
+    this.scrollPosition = this.ScrollPosition.generateScrollPosition();
+    this.endScrollPosition = this.ScrollPosition.endScrollPosition;
+    this.setDirectionInfo();
+    return this;
   }
   scrollEventUpdate() {
-    this.update()
+    this.update();
 
-    if(this.updateFunction) {
-      this.updateFunction(this)
+    if (this.updateFunction) {
+      this.updateFunction(this);
     } else {
       this.functions?.forEach(([func, scrollPosition]) => {
         func(
-          scrollPosition ?
-            Object.assign({}, this, { scrollPosition: scrollPosition.generateScrollPosition() }) :
-            this
-        )
-      })
+          scrollPosition
+            ? Object.assign({}, this, {
+                scrollPosition: scrollPosition.generateScrollPosition(),
+              })
+            : this
+        );
+      });
     }
 
-    requestAnimationFrame(this.scrollEventUpdate.bind(this))
+    requestAnimationFrame(this.scrollEventUpdate.bind(this));
   }
   update() {
-    this.scrollPosition = this.ScrollPosition.generateScrollPosition()
-    
-    this.endScrollPosition = this.ScrollPosition?.endScrollPosition
+    this.scrollPosition = this.ScrollPosition.generateScrollPosition();
+
+    this.endScrollPosition = this.ScrollPosition?.endScrollPosition;
 
     // @ts-ignore
+    // prettier-ignore
     this.stageSize = this.stage[`inner${this.stageSizeName}`] || this.stage[`client${this.stageSizeName}`]
     // @ts-ignore
+    // prettier-ignore
     this.contentSize = this.stage[`scroll${this.stageSizeName}`] || document.documentElement[`scroll${this.stageSizeName}`]
   }
 
   setDirectionInfo() {
-    this.directionPositionName = this.direction === 'y' ? 'Top' : 'Left' 
-    this.stageSizeName = this.direction === 'y' ? 'Height' : 'Width' 
+    this.directionPositionName = this.direction === 'y' ? 'Top' : 'Left';
+    this.stageSizeName = this.direction === 'y' ? 'Height' : 'Width';
   }
 }
 
 export class ScrollPosition {
-  stage: Stage
-  targetPercentage: number
-  threshold?: number
-  stageSize: number
-  direction: Direction
+  stage: Stage;
+  targetPercentage: number;
+  threshold?: number;
+  stageSize: number;
+  direction: Direction;
 
-  scrollPosition: number
-  endScrollPosition: number
-  scrollName: ScrollName
+  scrollPosition: number;
+  endScrollPosition: number;
+  scrollName: ScrollName;
 
   constructor(opt: ScrollStatus) {
-    this.stage = opt.stage
-    this.direction = opt.direction
-    this.stageSize = opt.stageSize
-    this.targetPercentage = opt.targetPercentage || 0.2
-    this.threshold = opt.threshold || 0
-    this.scrollName = this.stage === window ? `page${this.direction.toUpperCase() as 'Y' | 'X'}Offset` : `scroll${opt.directionPositionName}`
-    const scrollPosition = this.getScrollPosition()
-    this.scrollPosition = scrollPosition // 実際にスクロール
-    this.endScrollPosition = scrollPosition // 最後スクロールが止まる位置
+    this.stage = opt.stage;
+    this.direction = opt.direction;
+    this.stageSize = opt.stageSize;
+    this.targetPercentage = opt.targetPercentage || 0.2;
+    this.threshold = opt.threshold || 0;
+    this.scrollName =
+      this.stage === window
+        ? `page${this.direction.toUpperCase() as 'Y' | 'X'}Offset`
+        : `scroll${opt.directionPositionName}`;
+    const scrollPosition = this.getScrollPosition();
+    this.scrollPosition = scrollPosition; // 実際にスクロール
+    this.endScrollPosition = scrollPosition; // 最後スクロールが止まる位置
   }
 
   getScrollPosition() {
-    const stageThreshold = (this.stageSize || 0) * (this.threshold || 0)
+    const stageThreshold = (this.stageSize || 0) * (this.threshold || 0);
     // @ts-ignore
-    return this.stage[this.scrollName] as number + stageThreshold
+    return (this.stage[this.scrollName] as number) + stageThreshold;
   }
   generateScrollPosition() {
-    const scrollPosition = this.getScrollPosition()
-    const offset = (scrollPosition - this.scrollPosition) * this.targetPercentage
-    this.scrollPosition += offset
-    this.endScrollPosition = scrollPosition
+    const scrollPosition = this.getScrollPosition();
+    const offset =
+      (scrollPosition - this.scrollPosition) * this.targetPercentage;
+    this.scrollPosition += offset;
+    this.endScrollPosition = scrollPosition;
 
-    return Math.round(this.scrollPosition * 100) / 100
+    return Math.round(this.scrollPosition * 100) / 100;
   }
 }
 
-export const Status = new ScrollStatus()
+export const Status = new ScrollStatus();
